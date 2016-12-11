6374c05e311c84813e67beb26ceae0631ed1a587
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2016-12-11 14:58:28 +0200

    New events for IntervalEmitter: 'add', 'remove', 'change'
    This functionality is currently present in `resin-cli-visuals`,
    but this is a better place for it.
    (See https://github.com/resin-io/resin-cli-visuals/blob/edeb543a5c83cb67b684564f7835e8a69dfeaf6a/lib/widgets/drive/drive-scanner.coffee)

--- tests/interval-emitter.spec.js
+++ tests/interval-emitter.spec.js
@@ -16,7 +16,7 @@ describe('IntervalEmitter', function() {
       this.drivesListStub.restore();
     });
 
-    it('should emit an empty array', function(done) {
+    it('should emit an empty array as `drives`', function(done) {
       const scanner = new drivelist.IntervalEmitter(1000);
       scanner.on('drives', function(drives) {
         m.chai.expect(drives).to.deep.equal([]);
@@ -27,9 +27,55 @@ describe('IntervalEmitter', function() {
       scanner.start();
     });
 
+    it('should emit an empty array as `change`', function(done) {
+      const scanner = new drivelist.IntervalEmitter(1000);
+      scanner.on('drives', function(drives) {
+        m.chai.expect(drives).to.deep.equal([]);
+        scanner.stop();
+        done();
+      });
+
+      scanner.start();
+    });
+
+    it('should should not emit `add`', function(done) {
+      const scanner = new drivelist.IntervalEmitter(10);
+      scanner.on('add', function() {
+        m.chai.expect.fail();
+      });
+
+      let ticks = 0;
+      scanner.on('drives', function() {
+        if (ticks > 2) {
+          scanner.stop();
+          done();
+        }
+        ticks += 1;
+      });
+
+      scanner.start();
+    });
+
+    it('should should not emit `remove`', function(done) {
+      const scanner = new drivelist.IntervalEmitter(10);
+      scanner.on('remove', function() {
+        m.chai.expect.fail();
+      });
+
+      let ticks = 0;
+      scanner.on('drives', function() {
+        if (ticks > 2) {
+          scanner.stop();
+          done();
+        }
+        ticks += 1;
+      });
+
+      scanner.start();
+    });
   });
 
-  describe('given available drives', function() {
+  describe('given available drives change over time', function() {
 
     const DRIVES0 = [ 'a', 'b' ];
     const DRIVES1 = [ 'b', 'c' ];
@@ -46,7 +92,7 @@ describe('IntervalEmitter', function() {
       this.drivesListStub.restore();
     });
 
-    it('should emit the drives', function(done) {
+    it('should emit the `drives` event', function(done) {
       const scanner = new drivelist.IntervalEmitter(10);
 
       let called = 0;
@@ -55,7 +101,7 @@ describe('IntervalEmitter', function() {
           m.chai.expect(drives).to.deep.equal(DRIVES0);
         } else if (called === 1) {
           m.chai.expect(drives).to.deep.equal(DRIVES1);
-        } else {
+        } else if (called === 2) {
           m.chai.expect(drives).to.deep.equal(DRIVES2);
           scanner.stop();
           done();
@@ -63,6 +109,160 @@ describe('IntervalEmitter', function() {
         called += 1;
       });
 
+      scanner.start();
+    });
+
+    it('should emit the `add` event', function(done) {
+      const scanner = new drivelist.IntervalEmitter(10);
+
+      let called = 0;
+      scanner.on('add', function(drives) {
+        if (called === 0) {
+          m.chai.expect(drives).to.deep.equal(DRIVES0);
+        } else if (called === 1) {
+          m.chai.expect(drives).to.deep.equal([ 'c' ]);
+          scanner.stop();
+          done();
+        }
+        called += 1;
+      });
+      scanner.start();
+    });
+
+    it('should emit the `remove` event', function(done) {
+      const scanner = new drivelist.IntervalEmitter(10);
+
+      let called = 0;
+      scanner.on('remove', function(drives) {
+        if (called === 0) {
+          m.chai.expect(drives).to.deep.equal([ 'a' ]);
+        } else if (called === 1) {
+          m.chai.expect(drives).to.deep.equal([ 'b' ]);
+          scanner.stop();
+          done();
+        }
+        called += 1;
+      });
+      scanner.start();
+    });
+
+    it('should emit the `change` event', function(done) {
+      const scanner = new drivelist.IntervalEmitter(10);
+
+      let called = 0;
+      scanner.on('change', function(newDrives, oldDrives) {
+        if (called === 0) {
+          m.chai.expect(newDrives).to.deep.equal(DRIVES0);
+          m.chai.expect(oldDrives).to.deep.equal(null);
+        } else if (called === 1) {
+          m.chai.expect(newDrives).to.deep.equal(DRIVES1);
+          m.chai.expect(oldDrives).to.deep.equal(DRIVES0);
+        } else if (called === 2) {
+          m.chai.expect(newDrives).to.deep.equal(DRIVES2);
+          m.chai.expect(oldDrives).to.deep.equal(DRIVES1);
+          scanner.stop();
+          done();
+        }
+        called += 1;
+      });
+      scanner.start();
+    });
+  });
+
+  describe('given available drives do not change over time', function() {
+    const DRIVES0 = [ 'a', 'b' ];
+    const DRIVES1 = [ 'b', 'a' ];
+
+    beforeEach(function() {
+      this.drivesListStub = m.sinon.stub(drivelist, 'list');
+      this.drivesListStub.onCall(0).yields(null, DRIVES0);
+      this.drivesListStub.onCall(1).yields(null, DRIVES1);
+      this.drivesListStub.onCall(2).yields(null, DRIVES1);
+    });
+
+    afterEach(function() {
+      this.drivesListStub.restore();
+    });
+
+    it('should emit `add` once', function(done) {
+      const scanner = new drivelist.IntervalEmitter(10);
+      let called = 0;
+      scanner.on('add', function() {
+        if (called === 0) {
+          called += 1;
+          return;
+        }
+        m.chai.expect.fail();
+      });
+
+      let ticks = 0;
+      scanner.on('drives', function() {
+        if (ticks > 1) {
+          scanner.stop();
+          done();
+        }
+        ticks += 1;
+      });
+
+      scanner.start();
+    });
+
+    it('should should not emit `remove`', function(done) {
+      const scanner = new drivelist.IntervalEmitter(10);
+      scanner.on('remove', function() {
+        m.chai.expect.fail();
+        done();
+      });
+
+      let ticks = 0;
+      scanner.on('drives', function() {
+        if (ticks > 1) {
+          scanner.stop();
+          done();
+        }
+        ticks += 1;
+      });
+
+      scanner.start();
+    });
+
+    it('should should emit `change` once', function(done) {
+      const scanner = new drivelist.IntervalEmitter(10);
+
+      let called = 0;
+      scanner.on('change', function() {
+        if (called === 0) {
+          called += 1;
+          return;
+        }
+        m.chai.expect.fail();
+      });
+
+      let ticks = 0;
+      scanner.on('drives', function() {
+        if (ticks > 1) {
+          scanner.stop();
+          done();
+        }
+        ticks += 1;
+      });
+
+      scanner.start();
+    });
+
+    it('should emit `drives`', function(done) {
+      const scanner = new drivelist.IntervalEmitter(10);
+      let called = 0;
+      scanner.on('drives', function(drives) {
+        if (called === 0) {
+          m.chai.expect(drives).to.deep.equal(DRIVES0);
+        } else if (called === 1) {
+          m.chai.expect(drives).to.deep.equal(DRIVES1);
+          scanner.stop();
+          done();
+        }
+        called += 1;
+      });
       scanner.start();
     });
   });
