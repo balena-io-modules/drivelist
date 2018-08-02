8706f927e78f143b82a5121f73ee64759673aefa
Robin Andersson <robin.andersson@digiexam.se>
2018-08-02 16:07:36 +0200

    fix(other): Updated copyright in file

--- src/darwin/list.mm
+++ src/darwin/list.mm
@@ -1,5 +1,6 @@
 /*
  * Copyright 2017 resin.io
+ * Copyright 2018 Robin Andersson <me@robinwassen.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
